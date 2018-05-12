43c51e088d77ca7718f07bfe4ecb53dc5fedbd78
sumit <sumitchauhancoe@gmail.com>
2018-05-12 15:44:34 +0530

    post user route/ user model from user schema and schema methods

---
+++ playground/hashing.js
@@ -0,0 +1,7 @@
+const { SHA256 } = require('crypto-js');
+const jwt = require('jsonwebtoken');
+var data = {
+    id: 10
+}
+var token = jwt.sign(data, '123abc');
+console.log(token);
--- server/model/user.js
+++ server/model/user.js
@@ -1,10 +1,60 @@
-var mongoose = require('mongoose');
-var User = mongoose.model('users', {
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+/**
+ * User:
+ * email : string
+ * password: string //hashing bcrypt
+ * tokens : [{access,token}]
+ */
+var UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
         minlength: 3,
-        trim: true
-    }
+        trim: true,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
+UserSchema.methods.generateAuthToken = function() {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString();
+    var payload = {
+        access,
+        token
+    };
+    user.tokens.push(payload);
+    return user.save()
+        .then(() => {
+            return token;
+        })
+};
+UserSchema.methods.toJSON = function() {
+    var user = this;
+    var userObj = user.toObject();
+    return _.pick(userObj, ['_id', 'email']);
+}
+var User = mongoose.model('users', UserSchema);
+
 module.exports = { User };
